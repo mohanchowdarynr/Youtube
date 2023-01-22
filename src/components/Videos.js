edb598fc34240c64fc29b8f1704ffb2553c00dca
mohanchowdarynr <chiranjeevichowdary7@gmail.com>
2023-01-22 13:35:59 +0530

    Initial iteration done

---
+++ src/components/Videos.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Stack, Box} from "@mui/material";
+import VideoCard from "./VideoCard";
+import ChannelCard from "./ChannelCard";
+import Loader from "./Loader";
+
+// import { ChannelCard, Loader, VideoCard } from "./";
+
+const Videos = ({ videos, direction }) => {
+  if(!videos?.length) return <Loader />;
+  
+  return (
+    <Stack direction={direction || "row"} flexWrap="wrap" justifyContent="start" alignItems="start" gap={1}>
+      {videos.map((item, idx) => (
+        <Box key={idx}>
+          {item.id.videoId && <VideoCard video={item} /> }
+          {item.id.channelId && <ChannelCard channelDetail={item} />}
+        </Box>
+      ))}
+    </Stack>
+    // <Typography>hello</Typography>
+  );
+}
+
+export default Videos;
